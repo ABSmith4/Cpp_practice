7189508c6a7bea8dbf9c014417689771e4fb9838
Ahmad Smith <Smith.Ahmad3@gmail.com>
2023-06-12 16:42:57 -0400

    first cpp program

---
+++ Date_6_12_23/for_loop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+//this program will utilize for loops to create multiplication, addition, and subtraction table respectively.
+int main() {
+  int num;
+  int stop;
+  std::cout<<"Number of table\n";
+  std::cin>>num;
+  std::cout<<"Stopping number\n";
+  std::cin>>stop;
+  std::cout<<"\n";
+
+  //for addition
+  for(int i = 1; i<=stop; i++) {
+    std::cout<<i<<"+"<<num<<"="<<i+num<<"\n";
+  }
+
+  std::cout<<"\n";
+  //for multiplication
+  for(int i = 1; i<=stop; i++) {
+    std::cout<<i<<"*"<<num<<"="<<i*num<<"\n";
+  }
+
+  std::cout<<"\n";
+  //for subtraction
+  for(int i = 1; i<=stop; i++) {
+    std::cout<<i<<"-"<<num<<"="<<i-num<<"\n";
+  }
+};
+
+
